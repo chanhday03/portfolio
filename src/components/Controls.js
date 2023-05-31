c7fd695b3adc0b1af2d4f09e82c28460e2e1f486
Chanhday03 <116981561+chanhday03@users.noreply.github.com>
2023-05-31 21:19:56 +0700

    add tooltip control menu

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,31 +1,31 @@
 const Constrols = (pathName) => {
   return /*html */ `
     <div class="controls">
-    <a href="/">
+    <a href="/" title="Home Page">
         <div class="control ${pathName === "home" ? "active-btn" : ""}">
             <i class="fas fa-home"></i>
         </div>
     </a>
 
-    <a href="/about">
+    <a href="/about" title="About Page">
         <div class="control ${pathName === "about" ? "active-btn" : ""}">
             <i class="fas fa-user"></i>
         </div>
     </a>
 
-    <a href="/portfolio">
+    <a href="/portfolio" title="Portfolio Page">
     <div class="control ${pathName === "portfolio" ? "active-btn" : ""}">
             <i class="fas fa-briefcase"></i>
     </div>
     </a>
 
-    <a href="/blogs">
+    <a href="/blogs" title="Blogs Page">
     <div class="control ${pathName === "blogs" ? "active-btn" : ""}">
             <i class="far fa-newspaper"></i>
     </div>
     </a>
     
-    <a href="/contact">
+    <a href="/contact" title="Contact Page">
     <div class="control ${pathName === "contact" ? "active-btn" : ""}">
             <i class="fas fa-envelope-open"></i>
     </div>
